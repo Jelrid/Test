ab31713670311fa97f4a2d233b2697f5dd594604
Илья Рейх <agnedonia17@gmail.com>
2023-10-02 03:13:09 +0000

    xdebugComlite

--- app/User.php
+++ app/User.php
@@ -1,18 +1,12 @@
 <?php
 namespace app;
-class User 
-{
- public $name;
- public $email;
- public $password;
- public function __construct($data) {
-$this->name = $data['name'];
-$this->email = $data['email'];
- }
- public function verify(){
-if ($this->name && $this->email){
-return true;
-}
-return false;
+class User
+{ 
+ public function verifyPassword(array $user){
+ 
+    if(strlen($user['password'])<4){
+        throw new LengthException('Количество символов в пароле (' .$user['password']. ') не соответствует требованиям.');
+    }
+    //
  }
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,12 +1,14 @@
 <?php
 require_once dirname(__FILE__).'/../User.php';
 use PHPUnit\Framework\TestCase;
+
 class UserTest extends TestCase
 {
-    public function testGetPassword()
+    public function testVerifyPassword()
     {
         $obj = new User();
-        $user['login'] = 'login';
-        $this->assertArrayHasKey('password', $obj->getPassword($user));
+        $user['password'] = '123';
+        $this->expectException(LengthException::class);
+        $obj->verifyPassword($user);
     }
 }
